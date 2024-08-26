e03f36ac2f5f8c64557c13fe0bce605cd2588c38
Manrick Lazaro <manrick.lazaro@escolar.ifrn.edu.br>
2024-08-26 20:03:41 -0300

    resolve questões 1005 e 1006

---
+++ begginer/1006.py
@@ -0,0 +1,7 @@
+value_a = float(input())
+value_b = float(input())
+value_c = float(input())
+
+results = ((value_a * 2) + (value_b * 3) + (value_c * 5)) / 10
+
+print('MEDIA = {:.1f}'.format(results))
