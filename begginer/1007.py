2d436daf987f65793a1770e19b532760027c68ff
Manrick Lazaro <manrick.lazaro@escolar.ifrn.edu.br>
2024-08-26 20:18:19 -0300

    resolve questões 1007 e 1008

---
+++ begginer/1007.py
@@ -0,0 +1,8 @@
+value_a = int(input())
+value_b = int(input())
+value_c = int(input())
+value_d = int(input())
+
+DIFERENCA = (value_a * value_b - value_c * value_d)
+
+print('DIFERENCA = {}'.format(DIFERENCA))
---
+++ begginer/1008.py
@@ -0,0 +1,8 @@
+number_func = int(input())
+value = int(input())
+hour = float(input())
+
+salary = value * hour
+
+print('NUMBER = {}'.format(number_func))
+print('SALARY = U$ {:.2f}'.format(salary))
